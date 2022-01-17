3e07cd86e7348af69ad6c1c99c3bc173c1322e50
Foad Ataei <f.138019.a@gmail.com>
2022-01-17 17:43:51 +0330

    Project definition and how it works.

--- apacheAnalyzor.sh
+++ apacheAnalyzor.sh
@@ -1,1 +1,6 @@
-#bin
+#!/bin/bash
+
+#Author: SeyedFoadAtaei, SSN: 9821833, Date: 1/17/2022
+#Project is a bash script that analyzes an Apache log file.
+#Various queries are defined so that the user can retrieve information from the relevant file.
+
