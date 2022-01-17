2d2cdde877d7e0c5047f31939e15f2679f721990
Foad Ataei <f.138019.a@gmail.com>
2022-01-17 19:02:09 +0330

    The second function displays the 10 most visited IPs, respectively. To do this, first find non-duplicate IPs with awk and finally get the number of visits to each. This list is stored in mostIPs.txt .

--- apacheAnalyzor.sh
+++ apacheAnalyzor.sh
@@ -13,5 +13,14 @@ function countOfParticularURL {
 	cat $log | awk '{ print $1 }'  |  sort | uniq | wc -l
 }
 
-
+#Function 2: Show the 10 most visited IPs in order.
+#First we count the non-duplicate IPs and then we display the top ten IPs in order.
+#To store information, the list of these IPs will be saved in the mostIPs.txt file.
+function showTenTopIPs {
+    cat $log | awk '{ print $1}' | sort | uniq | wc | awk '{print $1 " Finding non-duplicate IPs is over" }'
+    cat $log | awk -F\" '{ print $1 }'| wc | awk '{print "All of IPs : " $1}'
+    echo Top 10 IPs:
+    awk '{print count "times {" $1 "} IP is repeated."}' $log | sort | uniq -c  | sort -nr | head -n 10
+    awk '{print count "times {" $1 "} IP is repeated."}' $log | sort | uniq -c  | sort -nr | head -n 10 > mostIPs.txt
+}
 
