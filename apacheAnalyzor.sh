00c7a952797c582cc603837d255b13a1010ea192
Foad Ataei <f.138019.a@gmail.com>
2022-01-17 18:37:47 +0330

    The first function shows the number of hits of a particular url. To simplify this, awk has been used.

--- apacheAnalyzor.sh
+++ apacheAnalyzor.sh
@@ -4,3 +4,14 @@
 #Project is a bash script that analyzes an Apache log file.
 #Various queries are defined so that the user can retrieve information from the relevant file.
 
+clear
+#The definition of functions and how they work are stated in order.
+
+#Function 1: Show the number of hits of a particular url
+function countOfParticularURL {
+    echo Count of Particular URL:
+	cat $log | awk '{ print $1 }'  |  sort | uniq | wc -l
+}
+
+
+
